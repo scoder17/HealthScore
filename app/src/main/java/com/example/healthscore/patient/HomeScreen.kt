7bc070d4f7023ca65d7d7b5f401bc19e41533b2b
amanksh05 <amanksh05@gmail.com>
2023-12-16 23:28:39 +0530

    HomeScreen complete

--- app/src/main/java/com/example/healthscore/patient/HomeScreen.kt
+++ app/src/main/java/com/example/healthscore/patient/HomeScreen.kt
@@ -17,15 +17,17 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,9 +35,12 @@ import androidx.compose.ui.unit.sp
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
+    val preventionList by remember {
+        mutableStateOf(mutableListOf<String>())
+    }
     Column(
         modifier = Modifier
-            .verticalScroll(rememberScrollState())
+//            .verticalScroll(rememberScrollState())
             .padding(top = 65.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -91,6 +96,20 @@ fun HomeScreen(modifier: Modifier = Modifier) {
             }
         }
 
+
+
+        Text(
+            text = "Current Medication",
+            modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp),
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Start
+        )
+
+
+
         Card(
             modifier = modifier
                 .fillMaxWidth()
@@ -99,13 +118,14 @@ fun HomeScreen(modifier: Modifier = Modifier) {
             Column() {
                 Text(
                     text = "ACTIVE", color = Color.White,
-                    modifier = Modifier.padding(top=8.dp,end=8.dp)
+                    modifier = Modifier
+                        .padding(top = 8.dp, end = 8.dp)
                         .clip(RoundedCornerShape(8.dp))
                         .background(Color.Green)
                         .padding(5.dp)
                         .align(Alignment.End),
                     fontSize = 12.sp,
-                    style=MaterialTheme.typography.labelMedium
+                    style = MaterialTheme.typography.labelMedium
                 )
                 Row(
                     modifier = modifier.fillMaxWidth(),
@@ -116,7 +136,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
                         horizontalArrangement = Arrangement.SpaceEvenly,
-                        modifier = modifier.padding(start=16.dp, bottom = 16.dp, end = 16.dp)
+                        modifier = modifier.padding(start = 16.dp, bottom = 16.dp, end = 16.dp)
                     ) {
                         Column(
                             modifier
@@ -125,24 +145,50 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                             horizontalAlignment = Alignment.Start,
                             verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.Top)
                         ) {
-                            Text(text = "Name", fontWeight = FontWeight.Bold)
-                            Text(text = "Age", fontWeight = FontWeight.Bold)
-                            Text(text = "Gender", fontWeight = FontWeight.Bold)
-                            Text(text = "UID", fontWeight = FontWeight.Bold)
+                            Text(text = "Hospital ID", fontWeight = FontWeight.Bold)
+                            Text(text = "Hospital Name", fontWeight = FontWeight.Bold)
+                            Text(text = "Doctor", fontWeight = FontWeight.Bold)
+                            Text(text = "Symptoms", fontWeight = FontWeight.Bold)
                         }
                         Column(
                             modifier.padding(10.dp),
                             verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.Top),
                             horizontalAlignment = Alignment.Start,
                         ) {
-                            Text(text = "Aman Kumar", fontWeight = FontWeight.Medium)
-                            Text(text = "21", fontWeight = FontWeight.Medium)
-                            Text(text = "Male", fontWeight = FontWeight.Medium)
-                            Text(text = "1DS21CS030", fontWeight = FontWeight.Medium)
+                            Text(text = "HSP0123", fontWeight = FontWeight.Medium)
+                            Text(text = "Sagar Hospital", fontWeight = FontWeight.Medium)
+                            Text(text = "Dr. Adarsh", fontWeight = FontWeight.Medium)
+                            Text(text = "Cold", fontWeight = FontWeight.Medium)
                         }
                     }
                 }
             }
         }
+
+        Text(
+            text = "Doctor Notes",
+            modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp),
+            fontWeight = FontWeight.ExtraBold,
+            fontSize = 20.sp,
+            textAlign = TextAlign.Start
+        )
+        Card(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(16.dp), elevation = CardDefaults.cardElevation(defaultElevation = 3.dp)
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+
+                Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
+                    Text(text = "Prevention")
+                }
+            }
+        }
+
     }
 }
