fdbb854c5d736aa637701d6cab0397675479800b
Gautam Shorewala <123801344+GautamCoder4019k@users.noreply.github.com>
2023-12-16 22:53:33 +0530

    -improved some ui

--- app/src/main/java/com/example/healthscore/patient/HomeScreen.kt
+++ app/src/main/java/com/example/healthscore/patient/HomeScreen.kt
@@ -1,6 +1,7 @@
 package com.example.healthscore.patient
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,18 +10,25 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
@@ -89,7 +97,16 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                 .padding(16.dp), elevation = CardDefaults.cardElevation(defaultElevation = 3.dp)
         ) {
             Column() {
-                Text(text = "ACTIVE")
+                Text(
+                    text = "ACTIVE", color = Color.White,
+                    modifier = Modifier.padding(top=8.dp,end=8.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(Color.Green)
+                        .padding(5.dp)
+                        .align(Alignment.End),
+                    fontSize = 12.sp,
+                    style=MaterialTheme.typography.labelMedium
+                )
                 Row(
                     modifier = modifier.fillMaxWidth(),
                     verticalAlignment = Alignment.CenterVertically,
@@ -99,7 +116,7 @@ fun HomeScreen(modifier: Modifier = Modifier) {
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
                         horizontalArrangement = Arrangement.SpaceEvenly,
-                        modifier = modifier.padding(16.dp)
+                        modifier = modifier.padding(start=16.dp, bottom = 16.dp, end = 16.dp)
                     ) {
                         Column(
                             modifier
