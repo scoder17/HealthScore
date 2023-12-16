1bf1f613e8ee0cb8b4834358297c66f4d37dc44e
amanksh05 <amanksh05@gmail.com>
2023-12-17 00:53:17 +0530

    1st routing done

--- app/src/main/java/com/example/healthscore/patient/sign_in_patient.kt
+++ app/src/main/java/com/example/healthscore/patient/sign_in_patient.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.compose.HealthScoreTheme
+import com.example.healthscore.data.GlobalVariable
 import kotlinx.coroutines.launch
 
 
